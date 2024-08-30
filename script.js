d6bbe8992376c279f40b0202a52d16004970928d
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-08-30 09:40:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const getSum = () => {
 	   const prices = document.querySelectorAll('.price');
 	let totalPrice= 0;
 	   // Loop through each price and add it to the total
-    prices.forEach(price => {
-        totalPrice += parseFloat(price.textContent);
-    });
+    for( let price of totalPrice) {
+        totalPrice += Number(price.textContent);
+    };
 	
 	  const totalRow = document.createElement('tr');
     const totalCell = document.createElement('td');
 	
 	    // Set the text content to show the total price
-    totalCell.textContent = `Total Price: Rs.${totalPrice.toFixed(2)}`;
+    totalCell.textContent = `Total Price: Rs.${totalPrice.toFixed()}`;
     totalRow.appendChild(totalCell);
 
  
